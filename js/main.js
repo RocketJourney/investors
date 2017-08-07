c6d66f31ec216dd6a3983a1febdc4c2cad9a2025
Pablo Artee <pabloartee@gmail.com>
2017-08-06 18:15:06 -0700

    Tap to Zoom Image now working! :D (shout out to magnific-popup library)

--- js/main.js
+++ js/main.js
@@ -1,75 +1,74 @@
 $(document).ready(function() {
-    "use strict";
+  "use strict";
 
-    // Anchor Smooth Scroll
-    $('body').on('click', '.page-scroll', function(event) {
-        var $anchor = $(this);
-        $('html, body').stop().animate({
-            scrollTop: ($($anchor.attr('href')).offset().top - 80)
-        }, 1500, 'easeInOutExpo');
-        event.preventDefault();
-    });
+    $('.open-image-popup').magnificPopup({type:'image'});
 
-    // Intro Slider
-    $('#main-slider').flexslider({
-        animation: "fade",
-        slideshowSpeed: 3500,
-        controlNav: false,
-        directionNav: false
-    });
+  // Anchor Smooth Scroll
+  $('body').on('click', '.page-scroll', function(event) {
+    var $anchor = $(this);
+    $('html, body').stop().animate({
+      scrollTop: ($($anchor.attr('href')).offset().top - 80)
+    }, 1500, 'easeInOutExpo');
+    event.preventDefault();
+  });
 
-    // Laptop Slider
-    $('#laptop-slider').flexslider({
-        animation: "slide",
-        slideshowSpeed: 3500,
-        controlNav: false,
-        directionNav: false
-    });
+  // Intro Slider
+  $('#main-slider').flexslider({
+    animation: "fade",
+    slideshowSpeed: 3500,
+    controlNav: false,
+    directionNav: false
+  });
 
-    $('.ls-prev').on('click', function() {
-        $('#laptop-slider').flexslider('prev')
-        return false;
-    })
+  // Laptop Slider
+  $('#laptop-slider').flexslider({
+    animation: "slide",
+    slideshowSpeed: 3500,
+    controlNav: false,
+    directionNav: false
+  });
 
-    $('.ls-next').on('click', function() {
-        $('#laptop-slider').flexslider('next')
-        return false;
-    })
+  $('.ls-prev').on('click', function() {
+    $('#laptop-slider').flexslider('prev')
+    return false;
+  })
 
-    // Fullwidth Slider
-    $('#intro6-slider').flexslider({
-        animation: "slide",
-        slideshowSpeed: 5000,
-        animationSpeed: 1000,
-        controlNav: false,
-        directionNav: true
-    });
+  $('.ls-next').on('click', function() {
+    $('#laptop-slider').flexslider('next')
+    return false;
+  })
 
-    $('.is-prev').on('click', function() {
-        $('#intro6-slider').flexslider('prev')
-        return false;
-    })
+  // Fullwidth Slider
+  $('#intro6-slider').flexslider({
+    animation: "slide",
+    slideshowSpeed: 5000,
+    animationSpeed: 1000,
+    controlNav: false,
+    directionNav: true
+  });
 
-    $('.is-next').on('click', function() {
-        $('#intro6-slider').flexslider('next')
-        return false;
-    })
+  $('.is-prev').on('click', function() {
+    $('#intro6-slider').flexslider('prev')
+    return false;
+  })
 
-    // Scrollspy
-    $('body').scrollspy({
-        target: ".navbar",
-        offset: 105
-    })
+  $('.is-next').on('click', function() {
+    $('#intro6-slider').flexslider('next')
+    return false;
+  })
 
-    // Video Lightbox
-    $( '.swipebox-video' ).swipebox();
+  // Scrollspy
+  $('body').scrollspy({
+    target: ".navbar",
+    offset: 105
+  })
 })
 
 // Fixed Header
 $(window).scroll(function() {
-    var value = $(this).scrollTop();
-    if (value > 80)
-        $(".navbar-default").css("background", "#ffd900");
-    else
-        $(".navbar-default").css("background", "transparent");
+  var value = $(this).scrollTop();
+  if (value > 80)
+  $(".navbar-default").css("background", "#ffd900");
+  else
+  $(".navbar-default").css("background", "transparent");
 })
